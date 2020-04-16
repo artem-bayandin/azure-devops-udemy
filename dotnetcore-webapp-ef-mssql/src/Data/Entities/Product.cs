212b86d7525162ae6a8861e8315c064403c8348a
Artem Bayandin <bayandin.artem.official@gmail.com>
2020-04-16 23:52:59 +0300

    moved from another folders

---
+++ dotnetcore-webapp-ef-mssql/src/Data/Entities/Product.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace Data.Entities
+{
+    public class Product
+    {
+        [Key]
+        public Guid Id { get; set; }
+
+        [StringLength(100, MinimumLength = 3)]
+        public string Name { get; set; }
+
+        [Range(0, 10000)]
+        public double Price { get; set; }
+    }
+
+    public class Person
+    {
+        [Key]
+        public Guid Id { get; set; }
+
+        [StringLength(100, MinimumLength = 2)]
+        public string Name { get; set; }
+
+        [StringLength(100, MinimumLength = 2)]
+        public string Surname { get; set; }
+
+        [Range(0, 150)]
+        public int Age { get; set; }
+    }
+}
---
+++ dotnetcore-webapp-ef-mssql/src/Data/MyContext.cs
@@ -0,0 +1,35 @@
+﻿using Data.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Data
+{
+    public interface IMyContext
+    {
+        DbSet<Product> Products { get; set; }
+        DbSet<Person> People { get; set; }
+
+        Task<int> SaveChangesAsync(CancellationToken cancellationToken);
+    }
+
+    public class MyContext : DbContext, IMyContext
+    {
+        public virtual DbSet<Product> Products { get; set; }
+        public virtual DbSet<Person> People { get; set; }
+
+        public MyContext(DbContextOptions<MyContext> options) : base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfigurationsFromAssembly(typeof(MyContext).Assembly);
+
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
